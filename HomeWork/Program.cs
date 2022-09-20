02c2d4745c3ffae3f9b1e26fae497755a8a089e6
Oleg_bul <kokseg63@gmail.com>
2022-09-20 15:01:01 +0400

    tasks complite

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,1 +1,54 @@
 ﻿
+int SecondDigit(int secondx)
+{
+    Console.WriteLine("Введите трехзначное число");
+    int a = int.Parse(Console.ReadLine());
+    secondx = (a / 10 % 10);
+    {
+        Console.WriteLine("Вторая цифра вашего числа " +  secondx);
+    }    
+    return secondx;
+}
+int secondx = SecondDigit(0);
+//................................................................................
+
+
+int thirdDigit(int third)
+{
+    
+    Console.WriteLine("Введите трехзначное число");
+    int a = int.Parse(Console.ReadLine());
+    if(a>99)
+    {
+        third = (a % 100 % 10);
+        {
+        Console.WriteLine("Третья цифра вашего числа " +  third);
+        }  
+    } 
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }     
+    return third;
+}
+int second = thirdDigit(0);
+//...................................................................................
+
+int weekendDay(int weekend)
+{
+    
+    Console.WriteLine("Введите цифру (от 1 до 7) обозначающую день недели");
+    int x = int.Parse(Console.ReadLine());
+    if(x>5)
+    {
+        
+        Console.WriteLine(x + " ваш день недели выходной");
+         
+    } 
+    else
+    {
+        Console.WriteLine(x + " ваш день недели не выходной");
+    }     
+    return x;
+}
+int weekend = weekendDay(0);
